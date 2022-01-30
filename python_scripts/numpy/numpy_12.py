3ce995a5c5ff261208fcc9418107efdab7e59b53
jyothischandran1597 <57825159+jyothischandran1597@users.noreply.github.com>
2022-01-30 19:39:57 +0530

    Delete python_scripts/numpy directory

--- python_scripts/numpy/numpy_12.py
+++
@@ -1,9 +0,0 @@
-# Identity matrix (N*N)
-
-import numpy as np
-
-print("Identity matrix (N*N)\n")
-
-print(np.eye(2),'\n')
-print(np.eye(3),'\n')
-print(np.eye(5),'\n')
--- python_scripts/numpy/numpy_4.py
+++
@@ -1,9 +0,0 @@
-# Array from a tuple
-
-import numpy as np
-
-tup = (10, 20, 30)
-print("Tuple: ",tup)
-arr = np.array(tup)
-
-print("Array from tuple: ",arr)
